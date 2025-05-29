0b9adb1042845b2bd0ba813c8fdf9219a5a63a0d
gaspar <airtontunni@gmail.com>
2025-05-28 21:24:57 -0300

    configurando o ambiente

---
+++ knexfile.js
@@ -0,0 +1,53 @@
+require("dotenv").config()
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT),
+    },
+    migrations: {
+      directory: "./migrations",
+      tableName: "knex_migrations",
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "empreiteira_lk",
+      user: "gaspar",
+      password: "0405",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "empreiteira_lk",
+      user: "gaspar",
+      password: "0405",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+}
